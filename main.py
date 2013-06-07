214ed9fac3a583bf994b1df9e1b6a07d24c6bace
Jonathan Leake <jleake527@gmail.com>
2013-06-07 18:21:24 -0500

    Restructured run/main files to put the curses wrapper in the 'wrapper' directory.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import curses;
+from wrapper.wrapper import curses_wrapper;
 from wrapper.cg import CursesGraphics;
-from run import main;
+from game import *;
 
-# Calls the "real" main function in the 'run' module (see 'run.py').
-def _main_wrapper(stdscr):
-	cg = CursesGraphics(stdscr);
-	main(cg);
+def main(cg):
+	for n in range(1, 30):
+		cg.draw('#', (CursesGraphics.RED, CursesGraphics.BLACK), [(n, 1), (n, 2), (n, 3), (n+1, 3)]);
+		cg.refresh();
+		cg.wait_for_key(1000);
+		cg.erase([(n, 1), (n, 2), (n, 3), (n+1, 3)]);
 
-# Wrapper for the main function does setup and cleanup for curses.
-curses.wrapper(_main_wrapper);
+# Call the main curses wrapper function to start things off. The parameter refers to the main function to be run after curses initialization. (In this case, this is the above "main" function.)
+curses_wrapper(main);
--- run.py
+++
@@ -1,9 +0,0 @@
-from wrapper.cg import CursesGraphics;
-from game import *;
-
-def main(cg):
-	for n in range(1, 30):
-		cg.draw('#', (CursesGraphics.RED, CursesGraphics.BLACK), [(n, 1), (n, 2), (n, 3), (n+1, 3)]);
-		cg.refresh();
-		cg.wait_for_key(1000);
-		cg.erase([(n, 1), (n, 2), (n, 3), (n+1, 3)]);
