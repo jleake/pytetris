13c6c0edaf061afc682e2d9d9799fc13c58749ea
Jonathan Leake <jleake527@gmail.com>
2013-06-10 22:40:56 -0500

    Added Piece class skeleton.

--- src/game.py
+++ src/game.py
@@ -5,3 +5,20 @@ class Block:
 		self.char = character
 		self.color = color
 
+
+class Piece:
+
+	# TODO: absolute points or based on pivot?
+	def __init__(self, points, pivot, character, color):
+		pass
+
+	def move_down():
+		pass
+
+	def rotate():
+		pass
+
+	# TODO: combine the 'move_down' and 'move_side' methods perhaps?
+	def move_side(base, direction):
+		pass
+
