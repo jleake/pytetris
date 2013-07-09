054202517f22f87c341a3134fb85d0124be89924
Michael Simmons <MikeSimmons@unknownb88d1205cf64.gateway.pace.com>
2013-07-08 22:15:24 -0500

    Scoped out move method of Piece

--- src/game.py
+++ src/game.py
@@ -8,17 +8,35 @@ class Block:
 
 class Piece:
 
-	# TODO: absolute points or based on pivot?
+	# Mike: "central" point from which others extend.
+	# Mike: thus when you move down it just shifts central point
+	# Mike: and when you rotate you merely shift orientation around central point.
+	# Mike: The also allows for easy creation of different pieces as you just have to make the points.
 	def __init__(self, points, pivot, character, color):
 		pass
+		self.pivot = pivot 
+		#self.points = (block,block,block...)
 
-	def move_down():
+	def move(base, direction): 
 		pass
+		if direction = #keystroke down and pivot(1) + 1 != floor.location:
+			self.pivot(1) = pivot(1) + 1
+		elif direction = #keystroke down and pivot (1) + 1 = floor.location:
+			self.hit = true #boolean to test if this piece has already hit the base once (will be absorbed during second hit)
+			#run method from floor class to absorb this piece
+		elif direction = #keystroke right and pivot(2) + 1 != floor.location:
+			self.pivot(2) = pivot(2) + 1
+		elif direction = #keystroke right and pivot(2) + 1 = floor.location:
+			break #if it is rotating into base, it doesn't rotate.
+		elif direction = #keystroke left and pivot(1) + 1 != floor.location:
+			self.pivot(2) = pivot(2) - 1
+		elif direction = #keystroke left and pivot(1) + 1 != floor.location:
+			break #if it is rotating into base, it doesn't rotate.
+			
 
-	def rotate():
+	def rotate(): 
 		pass
+		for point in self.points
+			self.point = point + 1
 
-	# TODO: combine the 'move_down' and 'move_side' methods perhaps?
-	def move_side(base, direction):
-		pass
 
