7797eedd078f4b0fb19febcbf1db4da12f56022d
Jonathan Leake <jleake527@gmail.com>
2013-07-15 23:05:51 -0500

    Added Floor class skeleton.

--- src/game.py
+++ src/game.py
@@ -13,12 +13,10 @@ class Piece:
 	# Mike: and when you rotate you merely shift orientation around central point.
 	# Mike: The also allows for easy creation of different pieces as you just have to make the points.
 	def __init__(self, points, pivot, character, color):
-		pass
 		self.pivot = pivot 
 		#self.points = (block,block,block...)
 
-	def move(base, direction): 
-		pass
+	def move(self, base, direction):
 		if direction = #keystroke down and pivot(1) + 1 != floor.location:
 			self.pivot(1) = pivot(1) + 1
 		elif direction = #keystroke down and pivot (1) + 1 = floor.location:
@@ -34,9 +32,25 @@ class Piece:
 			break #if it is rotating into base, it doesn't rotate.
 			
 
-	def rotate(): 
-		pass
+	def rotate(self):
 		for point in self.points
 			self.point = point + 1
 
 
+# Jono: Mike - I liked your use of 'floor' instead of 'base'.
+class Floor:
+
+	def __init__(self):
+		pass
+
+	def delete_rows(self):
+		pass
+
+	def add_piece(self, piece):
+		pass
+
+	def is_piece_hit(self, piece):
+		pass
+
+	def is_game_over(self):
+		pass
