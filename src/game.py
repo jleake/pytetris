f4af6e352fa5867817f65c8cda078f947c005803
Michael Simmons <MikeSimmons@108-196-152-18.lightspeed.austtx.sbcglobal.net>
2013-07-16 00:48:15 -0500

    changed move

--- src/game.py
+++ src/game.py
@@ -8,10 +8,15 @@ class Block:
 
 class Piece:
 
+# Mike: TODO: arms... they are each a block so they need two locations.
+			# They are fixed locations around the pivot.  We just don't
+			# know which ones the random number generator will choose
+			# So if it is this kind of piece rotate it this way.  elif this...
 	# Constants for 'move' function.
 	DOWN = 1
 	RIGHT = 2
 	LEFT = 3
+	UP = 4
 
 	# Mike: "central" point from which others extend.
 	# Mike: thus when you move down it just shifts central point
@@ -24,43 +29,51 @@ class Piece:
 	# Jono: returns a true/false value for whether or not the move was
 	# Jono: successful (i.e., collision with base)
 	def move(self, base, direction):
+	
 		# Store old pivot, in case movement hits the base.
 		old_pivot = self.pivot
+		old_arms = self.arms
 
-		# Since there are possible many, many locations that would
-		# need to be checked for base collision, let's just move the
-		# piece, then let the 'base' object check it. If there is a
-		# collision, we can just revert to the old pivot before we
-		# redraw the game. (see below)
 		if direction == DOWN:
 			self.pivot(1) = pivot(1) + 1
-
-		# continue if/else...
-		elif direction = #keystroke down and pivot (1) + 1 = floor.location:
-			self.hit = true #boolean to test if this piece has already hit the base once (will be absorbed during second hit)
-			#run method from floor class to absorb this piece
-		elif direction = #keystroke right and pivot(2) + 1 != floor.location:
-			self.pivot(2) = pivot(2) + 1
-		elif direction = #keystroke right and pivot(2) + 1 = floor.location:
-			break #if it is rotating into base, it doesn't rotate.
-		elif direction = #keystroke left and pivot(1) + 1 != floor.location:
+		elif direction == LEFT:
 			self.pivot(2) = pivot(2) - 1
-		elif direction = #keystroke left and pivot(1) + 1 != floor.location:
-			break #if it is rotating into base, it doesn't rotate.
+		elif direction = RIGHT:
+			self.pivot(2) = pivot(2) + 1
+		elif direction = UP:
+			self.rotate(self, base) #rotate to the right
 
 		# If we have a collision, revert to old pivot.
-		if base.is_piece_hit(self):
+		if self.hit(self, base) == 1:
 			self.pivot = old_pivot
+			self.arms = old_arms
+			# Mike: pause?
+			base.add_piece(base, self)
+		elif self.hit(self, base) == 2:	
+			self.pivot = old_pivot
+			self.arms = old_arms
 			return False
-
-		# If we made it here, the move was successful.
-		return True
+		else # If we made it here, the move was successful.
+			return True
+		
+	def hit(self, base) # Need location of new pivot, new "arms," and base.
+		# Mike: Take in direction search base matrix for location.
+		# Mike: If the new location of the piece hits a 1, throw a violation.
+		# Mike: Depending on the violation it either does not move or it gets absorbed.
+		if column = column:
+			# Mike: loop through arms and check base rows to see if there is a violation.
+			# Mike: if violation then (??? typically the game has a pause for you to change it move)
+			# Mike: loop thru arms loop thru columns.  If they == base location then return 1		
+		elif row = row:
+			# Mike: loop through arms and check base rows to see if there is a violation.
+			# Mike: if violation then (??? typically the game has a pause for you to change it move)
+			for arm in self.arms
+				# Mike: loop thru arms loop thru rows.  If they == base location then return 2
 
 	def rotate(self):
 		# TODO: matrix rotation
-
-		for point in self.points
-			self.point = point + 1
+		for arm in self.arms
+			self.arms(arm) = self.arms(arm) + 1
 
 
 # Jono: Back to 'Base'.
@@ -75,9 +88,6 @@ class Base:
 		pass
 
 	def add_piece(self, piece):
-		pass
-
-	def is_piece_hit(self, piece):
 		pass
 
 	def is_game_over(self):
